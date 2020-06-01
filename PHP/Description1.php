46cb5d8ae12aed0d1369d6ad05010476bec88bc4
SultanaOmar <64943200+SultanaOmar@users.noreply.github.com>
2020-06-01 03:43:25 +0300

    Create Description1.php

---
+++ PHP/Description1.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+ <html class="not-ie no-js">  
+<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
+		<title>Game #1 | I'm a Programmer</title>		
+		<link href="css/bootstrap.min.css" type="text/css" rel="stylesheet" media="screen">
+		<link href="css/style.css" type="text/css" rel="stylesheet" media="screen">
+		<link href="css/font-awesome.css" type="text/css" rel="stylesheet" media="screen">
+		<link href="css.css" type="text/css" rel="stylesheet">
+		<script src="js/interact.min.js"></script>
+		<script src="js/modernizr.js"></script>
+		<script src="js/respond.js"></script>
+		<style>
+                    .drag-drop-box {
+			display: inline-block;
+			min-width: 40px;
+			padding: 2em 0.5em;
+
+			color: #fff;
+			background-color: #29e;
+			border: solid 2px #fff;
+			width: 45%;
+		}
+		.white {
+			color: black;
+			background-color: #fff;
+		}
+		</style>
+	</head>
+        <body>
+            <div class="container" id="wrapper">
+
+			<header>
+				<a href="index.php" class="logo">
+					<img src="images/logo.png" alt="" />
+				</a>
+				<nav role="navigation">
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="fa">&#xf0c9;</span>
+						</button>
+					</div>
+
+					<?php include ('nav.php'); ?>
+					<div id="sun"></div>
+
+				</nav>
+				<div class="clearfix"></div>
+			</header>
+                        <section dir="rtl" id="content" class="col-lg-7">
+                            <div class="cloud2">
+                                <h1><span id="title">شرح مبسط عن اللعبة الأولى  </br> مرحبا بك</span></h1>
+                            </div>
+                            <div class="content-bg" style="background: #99c6df;">
+					<div id="desc">
+                                            <h2 class="entry-title">موقع أنا مبرمج يساعدك على تعلم بعض من المصطلحات والدوال في لغة الجافا</br>لغة الجافا: تم تطويرها لبناء تطبيقات للويب، السيرفرات، سطح المكتب، الهواتف والروبوتات وهي  تعمل على جميع أنظمة التشغيل مثل Windows, Mac OS, Linux, Unix, Android وتعتبر  من أشهر لغات البرمجة على الإطلاق</br>
+                                                في اللعبة الأولى من موقع أنا مبرمج تتعلم اول خطوة في كتابة برنامج في لغة الجافا وهي إنشاء وتعريف class بكتابة هذه الكلمات:</br>
+                                                -  public: معنى كلمة public هو أنه يمكن استدعاء الكلاس من أي مكان على المشروع، وفي وسط كلاسات أخرى، ولكي يتم الوصول إليها من خارج هذه الكلاس.  </br>
+                                                -class: وهي من الكلمات المحجوزة في الجافا وهو عبارة عن حاوية كبيرة تستطيع أن تحتوي كل الكود من متغيرات ودوال وكائنات إلخ..</br>
+                                                - يتبع كلمة class اسم الكلاس ويجب أن يبدأ بحرف كابتل مثل(A)</br>
+                                                ويحتوي class على الدالة (public static void main(String[] args</br>
+                                                ويحتوي أيضا على دالة الطباعة وهي من الدوال الجاهزة في لغة الجافا:</br>
+                                                ;()system.out.println</br>
+                                                نضع دائماً النصوص التي نريد طباعتها بين علامتي التنصيص "  ".</br>
+
+                                                مثال:</br>
+                                                }Public class people </br>
+                                               }( public static void static main(String[] args </br>
+                                                {{;("!system.out.println("Hello World
+                                                    </br>
+                                            </h2>
+                                            </br>
+                                            
+                                            
+                                            
+                                            
+
+
+
+
+
+
+
+
+                                            
+                                            
+                                            
+                                            
+                                            
+                                              <a href="game.php?l=1&g=1" class="button green pull-left">بدء اللعبة <span>&#xf137;</span></a>
+                                              <div id="buttons">
+						<a href="dashboard.php" class="button green pull-right"><span>&#xf138;</span> العودة للوحة التحكم</a>
+					</div>
+                                       
+                                        
+                        </section>
+                        
+          
+					</div>
+                                        
+                                        <?php include('footer.php'); ?>
+
+		<script src="js/jquery-1.10.1.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="js/custom.js"></script>
+		<script src="js.js"></script>
+		<script>
+            
+        </body>
+</html>
+
