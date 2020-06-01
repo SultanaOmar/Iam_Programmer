b1b4c36371f78499ababee7752a5049a81c08ac6
SultanaOmar <64943200+SultanaOmar@users.noreply.github.com>
2020-06-01 03:37:05 +0300

    Create about.php

---
+++ PHP/about.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html> 
+<html class="not-ie no-js"> 
+    <head> <meta charset="UTF-8"> <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0"> <title>About | I'm a Programmer</title> <link href="css/bootstrap.min.css" type="text/css" rel="stylesheet" media="screen"> 
+        <link href="css/style.css" type="text/css" rel="stylesheet" media="screen"> 
+        <link href="css/font-awesome.css" type="text/css" rel="stylesheet" media="screen">
+        <script src="js/modernizr.js"></script> 
+        <script src="js/respond.js"></script> 
+    </head> 
+    <body> 
+        <div class="container" id="wrapper">
+            <header> <a href="index.php" class="logo"> 
+                    <img src="images/logo.png" alt="" /> 
+                </a> <nav role="navigation"> <div class="navbar-header"> 
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse"> <span class="sr-only">Toggle navigation</span> <span class="fa">&#xf0c9;</span> </button> 
+                    </div> <?php include ('nav.php'); ?> <div id="sun"></div> 
+                </nav> <div class="clearfix"></div> 
+            </header> 
+            <section id="content" class="col-lg-7 col-md-8 col-sm-12"> <div class="cloud">
+                    <h1>عن الموقع</h1> </div> <div class="content-bg"> 
+                        <img src="images/wowslider/01.png" alt="" class="img-margin" /> 
+                        <h3 style="text-align:center;direction:rtl;">هل تريد ان تتعلم لغة البرمجة بطريقة ممتعه؟!<br /> هل ترغب في ان تكون مبرمج منذ الصغر؟!<br /> هنا لدينا كل ما تحتاجه لتحقق ذلك..<br /><br /> انا مبرمج هو موقع صمم لتعليم الطفل بعض من مهارات البرمجة كي تساعده في بداية طريقه ليصبح مبرمج .. </h3> 
+                        <h3 style="text-align:center;direction:rtl;"> يمكن الاستفاده من هذه المصادر لتطوير مهاراتك بلغه الجافا <br /> <a href="http://harmash.com/java/java-basic-syntax/" title="learn-java">أسلوب و مبادئ كتابة الكود في جافا</a> <br /> <a href="http://www.mjma3.com/programming/java/67-first-steps-in-java.html" title="first-steps-in-java">أول الخطوات في تعلم لغة البرمجة جافا</a> 
+                            <br /> كتاب اساسيات البرمجة بلغة الجافا – الجزء الأول- ل محمد محمود إبراهيم- جامعة الزعيم الأزهري<br /> البرمجة باستخدام لغة الجافا . أ. أمل زهران<br /> </h3> </div> </section> <div class="clearfix"></div> </div> <?php include('footer.php'); ?> <script src="js/jquery-1.10.1.min.js"></script> 
+                            <script src="js/bootstrap.min.js"></script> 
+                            <script src="js/custom.js"></script> 
+    </body> 
+</html>
